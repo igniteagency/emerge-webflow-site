e12e0408abc321bcd2d50e7c96f23af21e9e1b98
Sylvain Girard <sylvain97@gmail.com>
2026-04-27 10:32:41 +1000

    Update external link forwarder

--- src/utils/external-link.ts
+++ src/utils/external-link.ts
@@ -1,11 +1,23 @@
 const handleExternalLinks = (): void => {
-  // Select all anchor elements with data-external attribute set to "yes", "true", or "1"
-  const externalLinks = document.querySelectorAll<HTMLAnchorElement>(
-    'a[data-external="yes"], a[data-external="true"], a[data-external="1"]'
-  );
+  const externalLinks = document.querySelectorAll<HTMLAnchorElement>('a[data-external="yes"]');
 
   externalLinks.forEach((link) => {
     link.setAttribute('target', '_blank');
+  });
+
+  const detectExternalLinks = document.querySelectorAll<HTMLAnchorElement>('a[data-detect-external]');
+  
+  detectExternalLinks.forEach((link) => {
+    const href = link.getAttribute('href');
+    // Check if it's an absolute link (starts with http:// or https://)
+    if (href && /^https?:\/\//i.test(href)) {
+      // Check if it's pointing to a different domain
+      if (link.hostname !== window.location.hostname) {
+        link.setAttribute('target', '_blank');
+        // It's good practice to add rel="noopener noreferrer" for security when opening tabs
+        link.setAttribute('rel', 'noopener noreferrer');
+      }
+    }
   });
 };
 
